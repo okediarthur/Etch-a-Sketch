e3564ef0b9bdb1598281137aa6b0bf7aabb5f938
okediarthur <okediarthur@gmail.com>
2023-12-14 12:05:19 +0300

    Color Changes

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-console.log("Script is running!");
 document.addEventListener("DOMContentLoaded", function() {
     var container = document.getElementById("container");
     var resetButton = document.getElementById("resetButton");
+    var createGridButton = document.getElementById("createGridButton");
+    var numSquaresInput = document.getElementById("numSquares");
 
     //Function to handle mouseover event
     function handleMouseOver(event){
-        event.target.classList.add("hovered");
+        var randomColor = getRandomColor();
+        event.target.style.backgroundColor = randomColor;
+    }
+
+    //Function to generate a random RGB color
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for(var i = 0; i < 6; i++){
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
 
     //Function to handle reset button click
@@ -13,30 +25,47 @@ document.addEventListener("DOMContentLoaded", function() {
         //Remove the 'hovered' class from all grid items
         var gridItems = document.querySelectorAll('.grid-item');
         gridItems.forEach(function(item){
-            item.classList.remove('hovered');
+            // item.classList.remove('hovered');
+            item.style.backgroundColor = 'aquamarine';
         });
     }
     
-    // Looping to create rows
-    for(var i = 0; i < 16; i++){
-        //create a new row div
-        var row = document.createElement("div");
-        row.className = "grid-row";
+    //Function to handle create button click
+    function handleCreateGridButtonClick(){
+        //clear existing grid
+        container.innerHTML = '';
 
-        //looping to create a columns
-        for(var j = 0; j < 16; j++){
-            //create a new column
-            var cell = document.createElement("div");
-            cell.className = "grid-item";
+        //Get the number of sqaures per side from the input
+        var numSqaures = parseInt(numSquaresInput.value, 10) || 16;
 
-            //Add event listener for hover
-            cell.addEventListener("mouseover", handleMouseOver);
+         // Looping to create rows
+        for(var i = 0; i < numSqaures; i++){
+            //create a new row div
+            var row = document.createElement("div");
+            row.className = "grid-row";
 
-            //Append column div to row div
-            row.appendChild(cell);
+            //looping to create a columns
+            for(var j = 0; j < numSqaures; j++){
+                //create a new column
+                var cell = document.createElement("div");
+                cell.className = "grid-item";
+
+                //Add event listener for hover
+                cell.addEventListener("mouseover", handleMouseOver);
+
+                //Append column div to row div
+                row.appendChild(cell);
+            }
+            //append row div to container div
+            container.appendChild(row);
         }
-        //append row div to container div
-        container.appendChild(row);
     }
+    //Add event listener for reset button click
     resetButton.addEventListener("click", handleResetButtonClick);
+
+    //Add event listener for create grid button click
+    createGridButton.addEventListener("click", handleCreateGridButtonClick);
+
+    //Initial grid creation on page load
+    handleCreateGridButtonClick();
 });
