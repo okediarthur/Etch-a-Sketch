a6eac019506df1a8fc49f42e807c94e09de7ddaa
okediarthur <okediarthur@gmail.com>
2023-12-14 11:15:55 +0300

    hover feature

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ console.log("Script is running!");
 document.addEventListener("DOMContentLoaded", function() {
     var container = document.getElementById("container");
 
-    //looping to create rows
+    //Function to handle mouseover event
+    function handleMouseOver(event){
+        event.target.classList.add("hovered");
+    }
+    
+    // Looping to create rows
     for(var i = 0; i < 16; i++){
         //create a new row div
         var row = document.createElement("div");
         row.className = "grid-row";
 
-        //looping to create columns
+        //looping to create a columns
         for(var j = 0; j < 16; j++){
             //create a new column
             var cell = document.createElement("div");
             cell.className = "grid-item";
-            //append column div to row div
+
+            //Add event listener for hover
+            cell.addEventListener("mouseover", handleMouseOver);
+
+            //Append column div to row div
             row.appendChild(cell);
         }
-        //append the row div to the coontainer div
+        //append row div to container div
         container.appendChild(row);
-        console.log(container)
     }
-
 });
